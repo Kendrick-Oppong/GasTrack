f219b38851986dc8f97cdb1fcd3bc2b5527480b7
Kendrick <kendrickoppong94@gmail.com>
2024-07-22 13:22:48 +0000

    fix vercel deploy api

--- app/api/auth/createUser/route.ts
+++ app/api/auth/createUser/route.ts
@@ -3,32 +3,41 @@ import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-  if (!user || user === null || !user?.id) {
-    console.log("No user found");
-  }
+    if (!user || !user.id) {
+      console.log("No user found");
+      return NextResponse.json({ message: "No user found" }, { status: 401 });
+    }
 
-  let newDBUser = await prisma.user.findUnique({
-    where: {
-      kindeId: user?.id,
-    },
-  });
-
-  if (!newDBUser) {
-    newDBUser = await prisma.user.create({
-      data: {
-        email: user?.email ?? "",
-        firstName: user?.given_name ?? "",
-        lastName: user?.family_name ?? "",
-        profileImage:
-          user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`,
-
-        kindeId: user?.id ?? "",
-        phoneNumber: user?.phone_number ?? "",
+    let newDBUser = await prisma.user.findUnique({
+      where: {
+        kindeId: user.id,
       },
     });
+
+    if (!newDBUser) {
+      newDBUser = await prisma.user.create({
+        data: {
+          email: user.email ?? "",
+          firstName: user.given_name ?? "",
+          lastName: user.family_name ?? "",
+          profileImage:
+            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+          kindeId: user.id,
+          phoneNumber: user.phone_number ?? "",
+        },
+      });
+    }
+
+    return NextResponse.redirect(process.env.KINDE_SITE_URL as string);
+  } catch (err) {
+    console.error("An error occurred:", err);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-  return NextResponse.redirect(process.env.KINDE_SITE_URL as string);
 }

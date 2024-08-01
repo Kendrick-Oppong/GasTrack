45e4d3123ae2685ab923d1162aa285d818d8e365
Kendrick <kendrickoppong94@gmail.com>
2024-08-01 12:25:15 +0000

    update web hook

--- app/api/auth/createUser/route.ts
+++ app/api/auth/createUser/route.ts
@@ -3,41 +3,34 @@ import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    if (!user || !user.id) {
-      console.log("No user found");
-      return NextResponse.json({ message: "No user found" }, { status: 401 });
-    }
+  if (!user || !user.id) {
+    console.log("No user found");
+    return NextResponse.json({ message: "No user found" }, { status: 401 });
+  }
 
-    let newDBUser = await prisma.user.findUnique({
-      where: {
+  let newDBUser = await prisma.user.findUnique({
+    where: {
+      kindeId: user.id,
+    },
+  });
+
+  if (!newDBUser) {
+    newDBUser = await prisma.user.create({
+      data: {
+        email: user.email ?? "",
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
         kindeId: user.id,
+        phoneNumber: user.phone_number ?? "",
       },
     });
-
-    if (!newDBUser) {
-      newDBUser = await prisma.user.create({
-        data: {
-          email: user.email ?? "",
-          firstName: user.given_name ?? "",
-          lastName: user.family_name ?? "",
-          profileImage:
-            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-          kindeId: user.id,
-          phoneNumber: user.phone_number ?? "",
-        },
-      });
-    }
-
-    return NextResponse.redirect(process.env.KINDE_POST_LOGIN_REDIRECT_URL as string);
-  } catch (err) {
-    console.error("An error occurred:", err);
-    return NextResponse.json(
-      { message: "Internal Server Error" },
-      { status: 500 }
-    );
   }
+  return NextResponse.redirect(
+    process.env.KINDE_POST_LOGIN_REDIRECT_URL as string
+  );
 }

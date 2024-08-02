6ae53a8b0869774a17f329ffdbdd18f4fa0d12f9
Kendrick <kendrickoppong94@gmail.com>
2024-08-02 02:03:24 +0000

    update saveUserDetails action

--- lib/user/saveUserDetails.ts
+++ lib/user/saveUserDetails.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import prisma from "@/lib/db";
 
@@ -31,5 +33,3 @@ export async function saveUserDetails() {
 
   return { success: "User created" };
 }
-
-
